38a8f6f4d039fed8b54c7fec28ea4fe0bd0a5c19
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-03-13 07:02:12 +0700

    Initial commit

---
+++ lib/App/printenv/perl.pm
@@ -0,0 +1,13 @@
+package App::printenv::perl;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Print environment variables as seen by a Perl script
+
+=head1 SYNOPSIS
+
+See the included script L<printenv-perl>.
